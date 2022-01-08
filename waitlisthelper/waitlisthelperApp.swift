c1261fd1c107d2ab6b6700c6c624d8947c63c5e8
Jordan Yee <jordantyee@berkeley.edu>
2022-01-08 15:33:34 -0800

    Initial Commit

---
+++ waitlisthelper/waitlisthelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  waitlisthelperApp.swift
+//  waitlisthelper
+//
+//  Created by Jordan Yee on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct waitlisthelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
