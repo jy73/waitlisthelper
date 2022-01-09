08657d699f48bd5b0d197f87b76211cd9fce4b41
Jordan Yee <jordantyee@berkeley.edu>
2022-01-09 00:49:34 -0800

    finished with app

--- waitlisthelper/ContentView.swift
+++ waitlisthelper/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var waitlistPlace: Double = 0
     @State private var classSize: Double = 0
+    @State private var calculate = false
+    @State var probability = 0
+    @State var text = ""
     
     var body: some View {
         NavigationView {
@@ -30,7 +33,7 @@ struct ContentView: View {
                         Text("Place on Waitlist: \(waitlistPlace, specifier: "%.f")")
                         Spacer()
                     }
-                    Slider(value: $waitlistPlace, in: 0...100, step: 1)
+                    Slider(value: $waitlistPlace, in: 0...200, step: 1)
                         .padding()
                         .overlay(
                                 RoundedRectangle(cornerRadius: 15.0)
@@ -41,29 +44,64 @@ struct ContentView: View {
                         Text("Class Size: \(classSize, specifier: "%.f")")
                         Spacer()
                     }
-                    Slider(value: $classSize, in: 0...100, step: 1)
+                    Slider(value: $classSize, in: 0...1000, step: 1)
                         .padding()
                         .overlay(
                                 RoundedRectangle(cornerRadius: 15.0)
                                     .stroke(lineWidth: 2.0)
                                     .foregroundColor(Color.blue)
                             )
+                    NavigationLink(destination: ResultView(prob: $probability, feedback: $text), isActive: $calculate) { EmptyView() } .padding()
+                    
+                    Button("CALCULATE") {
+                        
+                        calculateProbability(waitlistPlace: Int(waitlistPlace), classSize: Int(classSize))
+                        text = getFeedbackText()
+                        calculate = true
+                    } .buttonStyle(GrowingButton())
 
-                    NavigationLink(destination: ResultView()) {
-                                Text("CALCULATE")
-                             }.buttonStyle(GrowingButton())
+//                    NavigationLink(destination: ResultView()) {
+//                                Text("CALCULATE")
+//                    }.simultaneousGesture(TapGesture().onEnded{
+//                        print("test")
+//                    })
+//                                .buttonStyle(GrowingButton())
                 } .padding()
                 
             }
-            .navigationBarTitle("")
+            .navigationBarTitle("Home")
             .navigationBarHidden(true)
         }
         
         }
+    func calculateProbability(waitlistPlace: Int, classSize: Int) {
+        let tenth = classSize / 10
+        if  (waitlistPlace <= tenth) {
+            probability = 100
+        } else if (waitlistPlace > tenth * 2) {
+            probability = 0
+        } else {
+            probability = 100 - Int(((Float(waitlistPlace - tenth) / Float(tenth))*100))
+        }
+        
     }
+    
+    func getFeedbackText() -> String {
+        for tuple in feedbackModel {
+            if probability >= tuple.0 {
+                return tuple.1
+            }
+        }
+        return "Error"
+    }
+}
 
 struct ResultView: View {
     @Environment(\.presentationMode) var presentation: Binding<PresentationMode>
+    @Binding var prob: Int
+    @Binding var feedback: String
+    
+    
     var body: some View {
 
         ZStack {
@@ -90,7 +128,13 @@ struct ResultView: View {
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .foregroundColor(Color.white)
-                    .padding()
+                Text("\(prob)%")
+                    .font(.system(size: 80, weight: .bold))
+                    .foregroundColor(Color.white)
+                Text("\(feedback)")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(Color.white)
+                Spacer()
                 Spacer()
             } .padding()
         }
---
+++ waitlisthelper/FeedbackModel.swift
@@ -0,0 +1,15 @@
+//
+//  FeedbackModel.swift
+//  waitlisthelper
+//
+//  Created by Jordan Yee on 1/9/22.
+//
+
+import Foundation
+
+let feedbackModel =
+    [(90, "You're good 😄"),
+     (60, "You're probably okay ☺️"),
+     (40, "Idk, it's like 50-50 😐"),
+     (20, "You're probably not getting off 😕"),
+     (0, "Rip 😔")]
