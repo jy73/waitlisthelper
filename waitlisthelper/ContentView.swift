1d1238146ebbb3453c49a994f94450b0721fbdc1
Jordan Yee <jordantyee@berkeley.edu>
2022-01-08 16:34:14 -0800

    setting up UI

--- waitlisthelper/ContentView.swift
+++ waitlisthelper/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.65, green: 1.0, blue: 0.99)
+                .ignoresSafeArea()
+            Text("WAITLIST HELPER")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .padding()
+        }
+        
     }
 }
 
